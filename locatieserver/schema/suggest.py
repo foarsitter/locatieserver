d0c042e800c22e80760034e7e1cd100df834ea10
jelmert <info@jelmert.nl>
2021-06-24 22:31:39 +0200

    feat: query params

--- locatieserver/schema/suggest.py
+++ locatieserver/schema/suggest.py
@@ -2,46 +2,54 @@ from __future__ import annotations
 
 from typing import Dict, List, Union
 
-from pydantic import BaseModel, Field
+from pydantic import Field
+
+from locatieserver.schema.base import LocatieserverBaseModel
 
 
-class Doc(BaseModel):
+class Doc(LocatieserverBaseModel):
     type: str
     weergavenaam: str
     id: str
     score: float
 
 
-class Response(BaseModel):
+class Response(LocatieserverBaseModel):
     num_found: int = Field(..., alias="numFound")
     start: int
     max_score: float = Field(..., alias="maxScore")
     docs: List[Doc]
 
 
-class Suggest(BaseModel):
+class Suggest(LocatieserverBaseModel):
     suggest: List[str]
 
 
-class Suggestion(BaseModel):
+class Suggestion(LocatieserverBaseModel):
     num_found: int = Field(..., alias="numFound")
     start_offset: int = Field(..., alias="startOffset")
     end_offset: int = Field(..., alias="endOffset")
     suggestion: List[str]
 
 
-class Collation(BaseModel):
+class Collation(LocatieserverBaseModel):
     collation_query: str = Field(..., alias="collationQuery")
     hits: int
     misspellings_and_corrections: List[str] = Field(..., alias="misspellingsAndCorrections")
 
 
-class Spellcheck(BaseModel):
+class Spellcheck(LocatieserverBaseModel):
     suggestions: List[Union[str, Suggestion]]
     collations: List[Union[str, Collation]]
 
 
-class SuggestResponse(BaseModel):
+class SuggestResponse(LocatieserverBaseModel):
     response: Response
     highlighting: Dict[str, Suggest]
     spellcheck: Spellcheck
+
+
+if __name__ == "__main__":
+    x = SuggestResponse.schema_json()
+
+    print(x)
