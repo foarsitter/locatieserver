b062286edbc3a6031c2996f53f38c64739343092
Jelmer Draaijer <info@jelmert.nl>
2021-06-24 08:51:45 +0200

    feat: scaffolding for the schema

---
+++ locatieserver/schema/base.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class LocatieserverBaseModel(BaseModel):
+    pass
+
+
+class SuggestResponse(LocatieserverBaseModel):
+    pass
+
+
+class LookupResponse(LocatieserverBaseModel):
+    pass
+
+
+class FreeResponse(LocatieserverBaseModel):
+    pass
