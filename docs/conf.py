8aa767702905784058b46dc9a640981b251079c9
jelmert <info@jelmert.nl>
2024-05-08 08:01:20 +0200

    Update pre-commit hooks & update formatting to the latest defaults

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 """Sphinx configuration."""
+
 project = "Locatieserver"
 author = "Jelmer Draaijer"
 copyright = "2023, Jelmer Draaijer"
--- src/locatieserver/client/__init__.py
+++ src/locatieserver/client/__init__.py
@@ -1,6 +1,5 @@
 """API methods of the locatieserver grouped in this module for clean imports."""
 
-
 from .free import free
 from .lookup import lookup
 from .suggest import suggest
